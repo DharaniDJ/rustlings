2c5d478f8cf0081f8e31210101d4306116af84c1
DharaniDJ <dharani56525@gmail.com>
2023-06-12 22:40:40 -0700

    completed functions/functions3.rs

--- exercises/functions/functions3.rs
+++ exercises/functions/functions3.rs
@@ -3,10 +3,10 @@
 // Execute `rustlings hint functions3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+// Fix: added argument in main call_me() fn
 
 fn main() {
-    call_me();
+    call_me(8);
 }
 
 fn call_me(num: u32) {
