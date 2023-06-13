d0e74c152f2fe97619bd8788763eb1032f317cb5
DharaniDJ <dharani56525@gmail.com>
2023-06-13 09:43:07 -0700

    completed primitive_types/primitive_types3.rs

--- exercises/primitive_types/primitive_types3.rs
+++ exercises/primitive_types/primitive_types3.rs
@@ -5,10 +5,10 @@
 // Execute `rustlings hint primitive_types3` or use the `hint` watch subcommand
 // for a hint.
 
-// I AM NOT DONE
+// Fix: added an array of size 1000
 
 fn main() {
-    let a = ???
+    let a = [1;1000];
 
     if a.len() >= 100 {
         println!("Wow, that's a big array!");
