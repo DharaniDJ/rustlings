3462fe30068af2b4bf702363226f8a66f9a62c8b
DharaniDJ <dharani56525@gmail.com>
2023-06-12 22:56:28 -0700

    completed quiz1.rs

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,16 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
+// Fix: added input and ouput type and if else condition
 
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(num: i32) -> i32{
+    if(num>40){
+        num*1
+    }else{
+        num*2
+    }
+}
 
 // Don't modify this function!
 #[test]
